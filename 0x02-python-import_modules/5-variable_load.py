57bc927f2c1fd245f0b1cbc29e57054939a92c49
Evans Sese <sese_evans@yahoo.com>
2023-06-08 15:27:35 -0700

    [Fix] Improved code

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import variable_load_5
-    variable_list = dir(variable_load_5)
-    for variable in variable_list:
-        if variable == 'a':
-            print(variable_load_5.a)
-            break
+    a = variable_load_5.a
+    print(a)
