34b422f1b90621deb3b271a00810e5c6e11ba9af
Evans Sese <sese_evans@yahoo.com>
2023-06-08 13:58:50 -0700

    [Feature] Added code

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    from calculator_1 import add, sub, mul, div
+    add_res = add(a, b)
+    sub_res = sub(a, b)
+    mul_res = mul(a, b)
+    div_res = div(a, b)
+    print("{} + {} = {}".format(a, b, add_res))
+    print("{} - {} = {}".format(a, b, sub_res))
+    print("{} * {} = {}".format(a, b, mul_res))
+    print("{} / {} = {}".format(a, b, div_res))
