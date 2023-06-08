71d1826bf4031a3661af4a042d54698de6ad7bea
Evans Sese <sese_evans@yahoo.com>
2023-06-08 13:32:52 -0700

    [Fix] Fixed code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-add_func = __import__('add_0')
-res = add_func.add(a, b)
-print("{} + {} = {}".format(a, b, res))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    add_func = __import__('add_0', fromlist=["add"])
+    res = add_func.add(a, b)
+    print("{} + {} = {}".format(a, b, res))
