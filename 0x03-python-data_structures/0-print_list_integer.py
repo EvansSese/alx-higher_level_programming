8e9a11324e2d9ca4ca792222a8338c11cd3331ca
Evans Sese <sese_evans@yahoo.com>
2023-06-10 06:06:59 -0700

    [Fix] Fixed code

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for my_int in my_list:
-            print("{}".format(my_int))
+def print_list_integer(my_list=[]):
+    for my_int in my_list:
+        print(my_int)
