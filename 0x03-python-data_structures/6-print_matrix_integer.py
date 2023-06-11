7b4441c89e434a31db3603a1badc4838503c9184
Evans Sese <sese_evans@yahoo.com>
2023-06-10 23:48:55 -0700

    [Feature] Added code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=' ')
-        print()
+    if matrix:
+        count = 1
+        size = len(matrix)
+        for i in matrix:
+            for j in i:
+                if count == size:
+                    print("{:d}".format(j), end='')
+                else:
+                    print("{:d}".format(j), end=' ')
+                count += 1
+            print()
+            count = 1
