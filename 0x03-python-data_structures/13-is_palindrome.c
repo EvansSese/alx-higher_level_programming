3c4a566dc6f2c8d277be4b8563eaa74e29838468
Evans Sese <sese_evans@yahoo.com>
2023-06-12 13:29:44 -0700

    [Fix] Fixed code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,39 +2,54 @@
 #include <stdlib.h>
 #include "lists.h"
 
-listint_t* reverseList(listint_t* head) {
-    listint_t* prev = NULL;
-    listint_t* current = head;
-    listint_t* next = NULL;
-
-    while (current != NULL) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-
-    return prev;
+/**
+ * reverseList - reverse all elements of a listint_t list
+ * @head: pointer to head of list
+ * Return: number of nodes
+ */
+listint_t *reverseList(listint_t *head)
+{
+listint_t *prev = NULL;
+listint_t *current = head;
+listint_t *next = NULL;
+while (current != NULL)
+{
+next = current->next;
+current->next = prev;
+prev = current;
+current = next;
+}
+return (prev);
 }
 
-int is_palindrome(listint_t** head){
-	if (*head == NULL) {
-		return 1;
-	}
-	listint_t* slowPtr = *head;
-	listint_t* fastPtr = *head;
-	while (fastPtr != NULL && fastPtr->next != NULL) {
-		slowPtr = slowPtr->next;
-		fastPtr = fastPtr->next->next;
-	}
-	listint_t* secondHalf = reverseList(slowPtr);
-	listint_t* firstHalf = *head;
-	while (secondHalf != NULL) {
-		if (firstHalf->n != secondHalf->n) {
-			return 0;
-		}
-		firstHalf = firstHalf->next;
-		secondHalf = secondHalf->next;
-	}
-	return 1;
+/**
+ * is_palindrome - check if list is palindrome
+ * @head: pointer to head of list
+ * Return: number of nodes
+ */
+int is_palindrome(listint_t **head)
+{
+if (*head == NULL)
+{
+return (1);
+}
+listint_t *slowPtr = *head;
+listint_t *fastPtr = *head;
+while (fastPtr != NULL && fastPtr->next != NULL)
+{
+slowPtr = slowPtr->next;
+fastPtr = fastPtr->next->next;
+}
+listint_t *secondHalf = reverseList(slowPtr);
+listint_t *firstHalf = *head;
+while (secondHalf != NULL)
+{
+if (firstHalf->n != secondHalf->n)
+{
+return (0);
+}
+firstHalf = firstHalf->next;
+secondHalf = secondHalf->next;
+}
+return (1);
 }
