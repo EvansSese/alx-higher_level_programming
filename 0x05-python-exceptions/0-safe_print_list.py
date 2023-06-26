cf3691065083a5e754fb1a0cd2317a9ee5fb27dd
Evans Sese <sese_evans@yahoo.com>
2023-06-26 13:24:15 -0700

    [Fix] Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
-        for item in my_list:
-            print("{}".format(item), end="")
+        for j in range(x):
+            print("{}".format(my_list[j]), end="")
             i += 1
-            if i == x:
-                break
     except IndexError:
+        print()
         return i
     print()
     return i
