dc037353e80fbafcbfee97f3cfe478dbfa81e308
Evans Sese <sese_evans@yahoo.com>
2023-06-26 13:21:22 -0700

    [Fix] Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             i += 1
             if i == x:
                 break
-        print()
     except IndexError:
         return i
+    print()
     return i
