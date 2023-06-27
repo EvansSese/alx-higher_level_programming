ceb52ea26b3595a0055831118c90f4a96fad3621
Evans Sese <sese_evans@yahoo.com>
2023-06-27 13:58:38 -0700

    [Feature] task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""define the square class"""
+
+class Square:
+    """this is the square class"""
+    def __init__(self, size=0):
+        """ This is the init method
+        it checks if size is of type int
+
+        Args:
+            size: Size of the square
+
+        """
+        if (type(size)) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
