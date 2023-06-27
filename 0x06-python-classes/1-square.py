c8715d1ebc30780f39d302f591922ea80d443447
Evans Sese <sese_evans@yahoo.com>
2023-06-27 13:36:12 -0700

    [Feature] Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define square class with size"""
+
+class Square:
+    """define init method to instanciate with size"""
+    def __init__(self, size):
+        """assign the value to the variable"""
+        self.__size = size
