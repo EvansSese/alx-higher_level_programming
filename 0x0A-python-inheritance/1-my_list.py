dca06092052d6b77a6b43e23b76783e56dbeda87
Evans Sese <sese_evans@yahoo.com>
2023-07-10 14:44:24 -0700

    [Fix] Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,14 @@
 
 
 class MyList(list):
-    """ Fuction to print sorted list """
+    """ Fuction to print sorted list
+    >>> my_list = MyList()
+    >>> my_list.append(1)
+    >>> my_list.append(2)
+    >>> print(my_list)
+    [1, 2]
+
+    """
     def print_sorted(self):
         sorted_list = sorted(self)
         return sorted_list
