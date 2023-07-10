879b18e313913602e53fe3506849a0e23839f31f
Evans Sese <sese_evans@yahoo.com>
2023-07-10 12:53:09 -0700

    [Feature] Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defines the base geometry class """
+
+
+class BaseGeometry:
+    """ Describes the BaseGeometry """
+    def area(self):
+        raise Exception("area() is not implemented")
