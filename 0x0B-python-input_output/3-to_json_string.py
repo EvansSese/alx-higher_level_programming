7b5566c194edc383ae9889cbd8464156b6a7ee25
Evans Sese <sese_evans@yahoo.com>
2023-07-11 12:00:53 -0700

    [Feature] Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,10 @@
 """ Defines function to convert to json """
 
 
+import json
+
+
 def to_json_string(my_obj):
+    """ Implement json string func """
     json_string = json.dumps(my_obj)
     return json_string
