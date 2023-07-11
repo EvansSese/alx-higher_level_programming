d0a356a9812c89de8377504710d9753e1ca36572
Evans Sese <sese_evans@yahoo.com>
2023-07-11 12:57:54 -0700

    [Feature] Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,6 +6,7 @@ import json
 
 
 def class_to_json(obj):
+    """ Class to JSON """
     json_dict = {}
     for attr_name, attr_value in obj.__dict__.items():
         if isinstance(attr_value, (list, dict, str, int, bool)):
