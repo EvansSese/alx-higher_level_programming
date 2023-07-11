583210f3752ac3e0df4a9cea3017454a5b84eede
Evans Sese <sese_evans@yahoo.com>
2023-07-11 10:56:18 -0700

    [Feature] Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """ Function  to read file """
     with open(filename, 'r', encoding="utf-8") as f:
-        contents = f.read()
+        for line in f:
+            print(line, end="")
