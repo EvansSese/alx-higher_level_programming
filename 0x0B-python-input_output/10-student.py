06c1f80efc7881771f0d1e29894c5489b21b1b09
Evans Sese <sese_evans@yahoo.com>
2023-07-11 13:23:12 -0700

    [feature] Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Class Student """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            attrs = self.__dict__.keys()
+        else:
+            attrs = filter(lambda attr: attr in self.__dict__, attrs)
+        json_dict = {}
+        for attr_name in attrs:
+            attr_value = getattr(self, attr_name)
+            if isinstance(attr_value, (list, dict, str, int, bool)):
+                json_dict[attr_name] = attr_value
+        return json_dict
