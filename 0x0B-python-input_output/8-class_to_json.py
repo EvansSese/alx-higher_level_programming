46de4ce080f1f5f82c8bf3a6cff3e6b72b91296c
Evans Sese <sese_evans@yahoo.com>
2023-07-11 12:56:36 -0700

    [Feature] Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Class to JSON """
+
+
+import json
+
+
+def class_to_json(obj):
+    json_dict = {}
+    for attr_name, attr_value in obj.__dict__.items():
+        if isinstance(attr_value, (list, dict, str, int, bool)):
+            json_dict[attr_name] = attr_value
+    return json_dict
