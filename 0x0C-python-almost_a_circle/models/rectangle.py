8ccaea6626d6566dc0f2b6bfd59879dd01e9996a
Evans Sese <sese_evans@yahoo.com>
2023-07-14 08:33:38 -0700

    [Fix] Task 2 & 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,10 +10,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -21,6 +21,12 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("width must be an integer")
+        
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = value
 
     @property
@@ -29,7 +35,13 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        sel.__height = value
+        if not isinstance(value, (int, float)):
+            raise TypeError("height must be an integer")
+    
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
 
     @property
     def x(self):
@@ -37,6 +49,12 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("x must be an integer")
+        
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = value
 
     @property
@@ -45,4 +63,10 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("y must be an integer")
+        
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
         self.__y = value
