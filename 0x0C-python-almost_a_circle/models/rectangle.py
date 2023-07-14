2be64eb9867e824ea60d2b3990ea590144055bcc
Evans Sese <sese_evans@yahoo.com>
2023-07-14 09:20:22 -0700

    [Feature] Task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,10 @@ class Rectangle(Base):
 
     def area(self):
         return (self.__width * self.__height)
+
+    def display(self):
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end='')
+            print("")
+
