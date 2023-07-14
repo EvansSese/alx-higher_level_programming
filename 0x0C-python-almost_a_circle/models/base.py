0cce56cd57d991b51f12506433a9360978dde3dd
Evans Sese <sese_evans@yahoo.com>
2023-07-14 05:42:38 -0700

    [Feature] Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the base class for the project
+"""
+
+
+class Base:
+    """ Defines the base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
