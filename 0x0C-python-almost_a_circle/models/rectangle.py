fc86532f53209236a498ca310db85ca20befb293
Evans Sese <sese_evans@yahoo.com>
2023-07-17 12:42:58 -0700

    [Fix] Fixed code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,12 +18,10 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    """ width getter function """
     @property
     def width(self):
         return self.__width
 
-    """ width setter function """
     @width.setter
     def width(self, value):
         if not isinstance(value, (int, float)):
@@ -34,12 +32,10 @@ class Rectangle(Base):
 
         self.__width = value
 
-    """ heigth getter function """
     @property
     def height(self):
         return self.__height
 
-    """ height setter function """
     @height.setter
     def height(self, value):
         if not isinstance(value, (int, float)):
@@ -50,12 +46,10 @@ class Rectangle(Base):
 
         self.__height = value
 
-    """ x getter function """
     @property
     def x(self):
         return self.__x
 
-    """ x setter function """
     @x.setter
     def x(self, value):
         if not isinstance(value, (int, float)):
@@ -66,12 +60,10 @@ class Rectangle(Base):
 
         self.__x = value
 
-    """ y getter function """
     @property
     def y(self):
         return self.__y
 
-    """ y setter function """
     @y.setter
     def y(self, value):
         if not isinstance(value, (int, float)):
@@ -82,11 +74,9 @@ class Rectangle(Base):
 
         self.__y = value
 
-    """ area function implementation """
     def area(self):
         return (self.__width * self.__height)
 
-    """ display function implementation """
     def display(self):
         for y in range(self.__y):
             print("")
@@ -97,12 +87,10 @@ class Rectangle(Base):
                 print("#", end='')
             print("")
 
-    """ String function definition """
     def __str__(self):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                 self.__x, self.__y, self.__width, self.__height))
 
-    """ update function implementation """
     def update(self, *args, **kwargs):
         if args:
             if len(args) >= 1:
@@ -119,7 +107,6 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-    """ return dictionary function """
     def to_dictionary(self):
         return {
                 'id': self.id,
