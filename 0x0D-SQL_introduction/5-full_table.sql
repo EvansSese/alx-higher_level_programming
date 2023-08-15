5d3dd9fbae25eaddb2d35d30f2f32e5262e6460c
Evans Sese <sese_evans@yahoo.com>
2023-08-15 13:51:17 -0700

    [feature] Task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- givces full eceep
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- list table details
+SHOW CREATE TABLE first_table;
