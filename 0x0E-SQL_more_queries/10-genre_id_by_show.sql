5eeece92a812d0d05a8c8b42c15522191e4b358e
Evans Sese <sese_evans@yahoo.com>
2023-08-16 14:01:37 -0700

    [Feature] Task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- list all shows that contain atleast 1 linked genre
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id ASC;
