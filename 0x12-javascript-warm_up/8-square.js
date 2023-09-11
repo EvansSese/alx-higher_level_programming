29b5b80be40f2b07efa33d0b4258ca2644966758
Evans Sese <sese_evans@yahoo.com>
2023-09-11 22:39:50 +0300

    [Feature] Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,9 +7,11 @@ if (args.length > 0) {
   const size = parseInt(args[0]);
   if (!isNaN(size)) {
     for (i = 0; i <= size; i++) {
+      let row = '';
       for (j = 0; j <= size; j++) {
-        console.log('X');
+        row += 'X';
       }
+      console.log(row);
     }
   } else {
     console.log('Missing size');
