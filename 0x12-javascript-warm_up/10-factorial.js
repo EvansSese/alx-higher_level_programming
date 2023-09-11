17d77fd1805a41277b499dc8c3158e343c4a5291
Evans Sese <sese_evans@yahoo.com>
2023-09-11 23:14:24 +0300

    [Feature] Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+const result = factorial(num);
+console.log(result);
