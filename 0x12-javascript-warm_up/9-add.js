109a95bdc1e95d707962774a6b4dae115c2a6771
Evans Sese <sese_evans@yahoo.com>
2023-09-11 13:00:43 -0700

    [Feature] Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,5 +5,5 @@ function add (a, b) {
 }
 
 const args = process.argv.slice(2);
-let result = add(parseInt(args[0]), parseInt(args[1]));
+const result = add(parseInt(args[0]), parseInt(args[1]));
 console.log(result);
