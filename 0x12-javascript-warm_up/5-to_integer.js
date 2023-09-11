a3b58a4c2d7400879115e3cba3c4f4c743346076
Evans Sese <sese_evans@yahoo.com>
2023-09-11 21:58:40 +0300

    [Feature] task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,10 +4,12 @@ const args = process.argv.slice(2);
 
 if (args.length > 0) {
   const firstArg = parseInt(args[0]);
-}
-
-if (!isNaN(firstArg)) {
-  console.log('My number: ' + firstArg);
+  
+  if (!isNaN(firstArg)) {
+    console.log('My number: ' + firstArg);
+  } else {
+    console.log('Not a number');
+  }
 } else {
   console.log('Not a number');
 }
