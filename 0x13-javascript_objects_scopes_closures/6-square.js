118e3f91b6cde3792d48c3530ac6781b0ef5f7cf
EvansSese <sese_evans@yahoo.com>
2023-09-12 17:40:51 +0300

    [Feature] Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,7 +11,7 @@ class Square extends ParentSquare {
     for (let i = 0; i < this.size; i++) {
       console.log(c.repeat(this.size));
     }
-    console.log('Print');
+    console.log(this.size);
   }
 }
 
