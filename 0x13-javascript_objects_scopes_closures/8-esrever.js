b9a91901b16a37b0965d34fe928052bb98d5094b
EvansSese <sese_evans@yahoo.com>
2023-09-12 18:36:43 +0300

    [Feature] Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -7,8 +7,8 @@ exports.esrever = function (list) {
   if (len > 1) {
     for (i = 0; i < len; i++) {
       temp = list[i];
-      list[i] = list[len - i];
-      list[len - i] = list[i];
+      list[i] = list[len - i - 1];
+      list[len - i - 1] = list[i];
     }
     return list;
   } else {
