1200167ec9f3a1eeebe609c3f941bf6fe451da6a
EvansSese <sese_evans@yahoo.com>
2023-09-12 17:14:45 +0300

    [Feature] Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,6 +3,10 @@
 const ParentSquare = require('./5-square');
 
 class Square extends ParentSquare {
+  constructor () {
+    super(size);
+  }
+  
   charPrint (c = 'X') {
     for (let i = 0; i < this.size; i++) {
       console.log(c.repeat(this.size));
