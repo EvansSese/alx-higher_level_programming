3976a650dfc6f4589c5d8e23292587a0403efd6b
EvansSese <sese_evans@yahoo.com>
2023-09-12 15:24:24 +0300

    [Feature] Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    let i, j;
+    for (i = 0; i < this.height; i++) {
+      let row = '';
+      for (j = 0; i < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
