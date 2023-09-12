7d8ef8f0ae301fc06e2ce20e1dca82b39cd1e3ab
EvansSese <sese_evans@yahoo.com>
2023-09-12 19:02:34 +0300

    [Feature] Task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  if (base > 1) {
+    return function convertToBase(num) {
+      if (num < base) {
+        return num.toString(base);
+      } else {
+        const quotient = Math.floor(num / base);
+        const remainder = num % base;
+        return convertToBase(quotient) + remainder.toString(base);
+      }
+    };
+  }
+};
