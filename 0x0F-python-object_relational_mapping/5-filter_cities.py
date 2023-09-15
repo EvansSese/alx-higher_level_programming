9cd22d9282614c0be50e64e0a2c525ad5324d02d
EvansSese <sese_evans@yahoo.com>
2023-09-15 16:34:02 +0300

    [Feature] Task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Defines function to filter cities"""
+
+
+import MySQLdb
+import sys
+
+
+def filter_cities(username, password, database_name, state_name):
+    """ Function to filter cities by state """
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=username,
+                             passwd=password,
+                             db=database_name)
+        cursor = db.cursor()
+        query = ("SELECT * FROM cities "
+                 "WHERE state_id = "
+                 "(SELECT state_id FROM states "
+                 "WHERE name = %s "
+                 "COLLATE utf8mb4_bin LIMIT 1) "
+                 "ORDER BY cities.id ASC")
+        cursor.execute(query, (state_name,))
+        cities = cursor.fetchall()
+        print(cities)
+    except MySQLdb.Error as e:
+        print("MySQL Error: ", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Wrong format")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        state_name = sys.argv[4]
+        filter_cities(username, password, database_name, state_name)
