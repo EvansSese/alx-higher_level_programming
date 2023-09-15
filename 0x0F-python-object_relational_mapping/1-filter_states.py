84acfa9472a4619b387cc935185d77dbbc730740
EvansSese <sese_evans@yahoo.com>
2023-09-15 12:44:51 +0300

    [Fix] Task 1 fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,24 +7,20 @@ import MySQLdb
 
 def list_states_with_n(username, password, database_name):
     """ Function to list states with N """
-    try:
-        db = MySQLdb.connect(host='localhost',
-                             port=3306,
-                             user=username,
-                             passwd=password,
-                             db=database_name)
-        cursor = db.cursor()
-        cursor.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-        )
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error: ", e)
-        sys.exit(1)
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database_name)
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    )
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
 
 
 if __name__ == "__main__":
