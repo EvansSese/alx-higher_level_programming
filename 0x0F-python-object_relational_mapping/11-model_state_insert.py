a4e0b08ba9cfc1c96228ee74a65606286487172e
Evans Sese <sese_evans@yahoo.com>
2023-09-17 01:13:22 +0300

    [Feature] Task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines function to insert"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def insert_state(username, password, database_name):
+    """Inserts state"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    louisiana = State('Louisiana')
+    session.add(louisiana)
+    session.commit()
+    state_id = session.query(State).filter(State.id == 'Louisiana').first()
+    print(state_id)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        pass
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        insert_state(username, password, database_name)
