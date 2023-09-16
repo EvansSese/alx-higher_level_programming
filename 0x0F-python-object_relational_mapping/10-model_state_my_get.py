08b5b79b8f8c70cdef998a10e60cb6f0ab2a6909
Evans Sese <sese_evans@yahoo.com>
2023-09-17 01:00:20 +0300

    [Feature] Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,7 +15,7 @@ def get_state(username, password, database_name, state_name):
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state_id = session.query(State.id).filter_by(name=state_name)
+    state_id = session.query(State.id).filter_by(name=state_name).first()
     if state_id is None:
         print('Not found')
     else:
