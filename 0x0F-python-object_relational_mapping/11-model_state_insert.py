d9ca761cbe6e1d6854942052007308af4349efa4
Evans Sese <sese_evans@yahoo.com>
2023-09-17 01:25:29 +0300

    [Feature] Task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -17,7 +17,7 @@ def insert_state(username, password, database_name):
     session = Session()
     louisiana = State(name='Louisiana')
     session.add(louisiana)
-    state_id = session.query(State).filter(State.id == 'Louisiana').first()
+    state_id = session.query(State.id).filter(State.name == 'Louisiana').first()
     print(state_id)
     session.commit()
     session.close()
