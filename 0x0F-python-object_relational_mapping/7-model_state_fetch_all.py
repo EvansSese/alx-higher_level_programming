7d4184c7eb42b200004657b891e8f6a6ec2d0266
Evans Sese <sese_evans@yahoo.com>
2023-09-16 22:51:35 +0300

    [Feature] Task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines function to fetch all states"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states(username, password, database_name):
+    """Lists all states"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(state.id, end=': ')
+        print(state.name)
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        pass
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        list_states(username, password, database_name)
