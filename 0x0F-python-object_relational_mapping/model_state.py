1c5b7434383767277ec3f7358fe9fd7271b82ae9
Evans Sese <sese_evans@yahoo.com>
2023-09-16 22:21:14 +0300

    [Feature] Task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Defines the state model """
+
+
+import sqlalchemy
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
