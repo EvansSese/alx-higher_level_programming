b4c360c7c0210e19b2c3d5921c855746d5b117c7
Evans Sese <sese_evans@yahoo.com>
2023-09-17 21:41:30 +0300

    [feature] Task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines the city model """
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """ Class that defines city model """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Defines the state model """
+
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class that defines state model """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
