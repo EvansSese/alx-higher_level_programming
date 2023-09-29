d39866789b60efb2dafce688f6f47bb8bf04c190
Evans Sese <sese_evans@yahoo.com>
2023-09-29 16:23:36 -0700

    [Feature] Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,6 +12,8 @@ def show_error(url):
     status_code = response.status_code
     if status_code >= 400:
         print(f"Error code: {status_code}")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
