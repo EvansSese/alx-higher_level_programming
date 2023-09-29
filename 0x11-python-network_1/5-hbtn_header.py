b3c004d935907e00ff566b6f436c34945dd3a3f0
Evans Sese <sese_evans@yahoo.com>
2023-09-29 15:55:58 -0700

    [Feature] Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Script to display header """
+
+
+import requests
+import sys
+
+
+def display_header(url):
+    """ Function to display header """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(f"{x_request_id}")
+        else:
+            print("X-Request-Id header not found in the response.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching URL: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    display_header(url)
