d7d1fab58cffee9dafa352cab5d5e0049d6c5efe
Evans Sese <sese_evans@yahoo.com>
2023-09-29 16:20:58 -0700

    [Feature] Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Script to show error """
+
+
+import requests
+import sys
+
+
+def show_error(url):
+    """ Function to show error """
+    response = requests.get(url)
+    status_code = response.status_code
+    if status_code >= 400:
+        print(f"Error code: {status_code}")
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    show_error(url)
