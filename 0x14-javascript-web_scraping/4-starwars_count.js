381dd5f5c02a5e7f05ded41f7c0e1051fe991e7a
Evans Sese <sese_evans@yahoo.com>
2023-10-24 23:11:28 +0300

    [Feature] Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.log('Usage: node ${process.argv[0]} <API_URL>');
+  process.exit(1);
+}
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(response.statusCode);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      const wedgeAntillesMovies = movieData.results.filter(movie =>
+        movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+      );
+      console.log('Number of movies with Wedge Antilles: ${wedgeAntillesMovies.length}');
+    } catch (parseError) {
+      console.error(parseError);
+    }
+  }
+});
