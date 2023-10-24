954911e252bca5b8ddf30dd390c014b2903c5101
Evans Sese <sese_evans@yahoo.com>
2023-10-24 23:58:19 +0300

    [Feature] Task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.log(`Usage: node ${process.argv[0]} <Movie_ID>`);
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(response.statusCode);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      console.log(`Characters in "${movieData.title}":`);
+      movieData.characters.forEach(character => {
+        request(character, (characterError, characterResponse, characterBody) => {
+          if (characterError) {
+            console.error(characterError);
+          } else if (characterResponse.statusCode !== 200) {
+            console.error(characterResponse.statusCode);
+          } else {
+            const characterData = JSON.parse(characterBody);
+            console.log(characterData.name);
+          }
+        });
+      });
+    } catch (parseError) {
+      console.error(parseError);
+    }
+  }
+});
